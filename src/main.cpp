e709d81485dd63ac31999a11995c74cfb9a34f2c
JoshDesmond <JoshDesmond@users.noreply.github.com>
2019-06-03 19:04:24 -0400

    Added shaders

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,46 @@
 #include <glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str(); //alternatively &source[0];
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char message[length]; // Did this used to give an error for allocating unknown size to stack? Why no Alloca needed?
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs); // should be doing detach instead of delete for debugging or something
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -28,17 +68,52 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = { // Array of three vertex positions
+            -.5f, -.5f,
+            0.0f, 0.5f,
+            0.5f, -.5f
+    };
+    // Note that a "vertex" in the context of graphics can mean more than just position, like,
+    // texture coordinates, normals, colors, binormals, tangents, etc.
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); //Instead of a return, gl will modify the buffer variable you provided a pointer to
+    // &buffer thus represents a type of ID for a buffer which we can use later.
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    // Notice there is no size specified. Next step is to add data to the buffer.
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (const void*) 0);
+
+    std::string vertexShader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) in vec4 position;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   gl_Position = position;\n"
+            "}\n";
+    std::string fragmentShader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) out vec4 color;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+            "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-.5f, -.5f);
-        glVertex2f(.0f, .5f);
-        glVertex2f(.5f, -.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3); // "This is the call we use when we don't have an index buffer" ??
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -46,6 +121,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
